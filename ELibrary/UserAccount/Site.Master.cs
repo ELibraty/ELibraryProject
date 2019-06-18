5e12972c5c6cd7985be51ba905c8c7439f4caa33
Dimitar <dim_kolev2002@abv.bg>
2019-06-18 17:52:48 +0300

    doing login form

--- ELibrary/UserAccount/Site.Master.cs
+++ ELibrary/UserAccount/Site.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace ELibrary
 {
-    public partial class Contact : Page
+    public partial class SiteMaster : MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
